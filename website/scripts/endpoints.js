a6755f3d107cfe64e9b57c71dfe9ed27a812f85f
Mason Scarbro <120221711+MasonScarbro@users.noreply.github.com>
2025-04-07 19:19:33 -0400

    Updated domain

--- website/scripts/endpoints.js
+++ website/scripts/endpoints.js
@@ -17,7 +17,7 @@ export async function validateUser(emailOrUsername, password) {
         const data = await response.json();
         
         if (!response.ok) throw new Error(data.error || "Invalid username or password");
-        window.location.href = "https://dulcet-baklava-36c54a.netlify.app/survey.html"; 
+        window.location.href = "https://itsc4155teamforge.netlify.app/survey.html"; 
         return data.user; // Ensure this returns the user properly
 
     } catch (error) {
@@ -124,7 +124,7 @@ export async function sendSurveyInfo(formObject) {
         if (response.ok) {
             alert("✅ Data updated successfully!");
             console.log(result);
-            window.location.href = "https://itsc4155teamforge.netlify.app//biography.html"; 
+            window.location.href = "https://itsc4155teamforge.netlify.app/biography.html"; 
         } else {
             alert("❌ Error updating data: " + result.error);
         }
